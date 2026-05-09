bcda78e28f6ac31bd317103e7a43520ddb4ab190
Abdallah Talal Abdallah Alja'Fari <aljafariabdullah2005@gmail.com>
2026-05-09 17:39:06 +0300

    Update gemeni.py

--- ir_project/gemeni.py
+++ ir_project/gemeni.py
@@ -1,11 +1,1 @@
-from google import genai
-
-# Initialize the client. 
-# It will automatically pick up your GEMINI_API_KEY environment variable.
-client = genai.Client(api_key="AIzaSyB4K5UWKQseNWT9jHMhXUf5xoZvhxiy1Zg")
-
-print("Models available for content generation:")
-for model in client.models.list():
-    # Filter specifically for models that support text/content generation
-    if "generateContent" in model.supported_actions:
-        print(f"- {model.name}")
+jjj
